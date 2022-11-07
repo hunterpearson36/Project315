8a160f768808fd4f48980ad3fb1a4fc061f6443d
hunter-pearson-36 <hunter-pearson_36@tamu.edu>
2022-11-07 13:00:39 -0600

    Added additional functionality to Customer GUI

--- client/src/Pages/Customer.js
+++ client/src/Pages/Customer.js
@@ -3,39 +3,66 @@ import {useNavigate} from "react-router-dom";
 
 function Customer() {
     let navigate = useNavigate();
-    const entrees = [
+    let total = 0;
+    let entrees = [
         {value: '', text: '--Choose an option--'},
         {value: 'Chicken Sandwich', text: 'Chicken Sandwich'},
         {value: 'Cobb Salad', text: 'Cobb Salad'},
         {value: 'Spicy Chicken Sandwich', text: 'Spicy Chicken Sandwich'},
       ];
     
-    const sides = [
+    let sides = [
         {value: '', text: '--Choose an option--'},
         {value: 'Small Fries', text: 'Small Fries'},
         {value: 'Medium Fries', text: 'Medium Fries'},
         {value: 'Large Fries', text: 'Large Fries'},
       ];
     
-    const desserts = [
+    let desserts = [
         {value: '', text: '--Choose an option--'},
         {value: 'Small Chocolate Shake', text: 'Small Chocolate Shake'},
         {value: 'Small Vanilla Shake', text: 'Small Vanilla Shake'},
         {value: 'Small Strawberry Shake', text: 'Small Strawberry Shake'},
       ];
 
-    const drinks = [
+    let drinks = [
         {value: '', text: '--Choose an option--'},
         {value: 'Small Iced Tea', text: 'Small Iced Tea'},
         {value: 'Medium Iced Tea', text: 'Medium Iced Tea'},
         {value: 'Large Iced Tea', text: 'Large Iced Tea'},
       ];
 
+      let data = [
+        {name: 'Spicy Chicken Sandwich'},
+        {name: 'Medium Fries'},
+        {name: 'Small Iced Tea'}
+      ]
+
+    function handleAdd(name){
+        data.push({name: {name}});
+        // figure out how to re render order table after adding element
+        // figure out how to re render total after updating 
+    }
+
     return (
         <div> 
             <div>
                 <label>Enter Your Name:</label><br/>
                 <input type="text" placeholder="Name" name="Name" /><br/><br/>
+
+                <table>
+                    <tr>
+                        <th>Order</th>
+                        </tr>
+                        {data.map((val, key) => {
+                        return (
+                            <tr key={key}>
+                            <td>{val.name}</td>
+                            </tr>
+                        )
+                        })}
+                    
+                </table><br/>
 
                 <label for = "entrees">Entrees</label><br/>
                 <select name="entrees" id="selectEntrees">
@@ -45,7 +72,13 @@ function Customer() {
                     </option>
                     ))}
                 </select>
-                <button>Add Entree to order</button><br/>
+                <button
+                    onClick={() => {
+                        handleAdd(document.getElementById("selectEntrees").value);
+                    }}
+                >
+                    Add Entree to order
+                </button><br/>
 
                 <label for = "sides">Sides</label><br/>
                 <select name="sides" id="selectSides">
@@ -55,7 +88,13 @@ function Customer() {
                     </option>
                     ))}
                 </select>
-                <button>Add Side to order</button><br/>
+                <button
+                    onClick={() => {
+                        handleAdd(document.getElementById("selectSides").value);
+                    }}
+                >
+                    Add Side to order
+                </button><br/>
 
                 <label for = "desserts">Desserts</label><br/>
                 <select name="desserts" id="selectDesserts">
@@ -65,7 +104,13 @@ function Customer() {
                     </option>
                     ))}
                 </select>
-                <button>Add Dessert to order</button><br/>
+                <button
+                    onClick={() => {
+                        handleAdd(document.getElementById("selectDesserts").value);
+                    }}
+                >
+                    Add Dessert to order
+                </button><br/>
 
                 <label for = "drinks">Drinks</label><br/>
                 <select name="drinks" id="selectDrinks">
@@ -75,13 +120,22 @@ function Customer() {
                     </option>
                     ))}
                 </select>
-                <button>Add Drink to order</button><br/><br/>
+                <button
+                    onClick={() => {
+                        handleAdd(document.getElementById("selectDrinks").value);
+                        // add to total
+                    }}
+                >
+                    Add Drink to order
+                </button><br/>
+                <br/>
 
-                <label>TOTAL: </label><br/><br/>
+                <label>TOTAL: ${parseFloat(total).toFixed(2)}</label><br/><br/>
 
                 <button
                 onClick={() => {
                     navigate("/order-placed");
+                    total = 0;
                   }}
                 >
                     Create Order
@@ -89,6 +143,7 @@ function Customer() {
                 <button
                 onClick={() => {
                     navigate("/");
+                    total = 0;
                   }}
                 >
                     Cancel Order
