1fbb0649716b106d998d4c4b5feb4f1898898ddb
lacarbo <lacarbo@tamu.edu>
2022-11-07 23:47:15 -0600

    moved manager.js to correct location

--- client/src/Pages/Manager.js
+++ client/src/Pages/Manager.js
@@ -1,10 +1,30 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 function Manager() {
+    let navigate = useNavigate();
     return (
         <div>
             Manager GUI
+            <button 
+                onClick={() => {
+                navigate("/manager/item_structures-table");
+                }}
+            >         
+            Go to item structures list
+            </button>
+
+            <button 
+                onClick={() => {
+                navigate("/manager/ingredients-table");
+                }}
+            >         
+            Go to ingredients list
+            </button>
         </div>
+
+          
+        
     );
 }
 
