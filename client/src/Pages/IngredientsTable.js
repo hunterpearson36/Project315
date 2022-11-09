87308262606c344f6f0feb5ff3bbdd59427b4561
lacarbo <lacarbo@tamu.edu>
2022-11-09 00:31:45 -0600

    filling in missing background files so react doesn't hate us

---
+++ client/src/Pages/IngredientsTable.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function IngredientsTable() {
+    return <div>Page is not set up yet</div>;
+}
+
+export default IngredientsTable;
---
+++ client/src/Pages/ItemStructures.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ItemStructures() {
+    return <div>Page is not set up yet</div>;
+}
+
+export default ItemStructures;
