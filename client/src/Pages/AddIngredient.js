e7cf53d0549cf83ce404062a78c53f8b56556410
hunter-pearson-36 <hunter-pearson_36@tamu.edu>
2022-11-13 17:32:55 -0600

    Added Ingredient creation

--- client/src/Pages/AddIngredient.js
+++ client/src/Pages/AddIngredient.js
@@ -5,10 +5,124 @@ import { sendQuery, sendUpdate } from "../modules/Query";
 const AddIngredient = () => {
 
     let navigate = useNavigate();
+    const [ingredients, setIngred] = useState([]);
+
+    const getIngred = async () => {
+        await sendQuery("SELECT * from ingredients where ingred_id < 2000 order by ingred_id;")
+          .then((response) => {
+            console.log("received response");
+            setIngred(response);
+          }).catch((error) => {
+            console.error(error.message);
+          })
+    }
+
+    const updateData = async (statement) => {
+        await sendUpdate(statement)
+          .then((response) => {
+            console.log("received response");
+          }).catch((error) => {
+            console.error(error.message);
+          });
+    }
+
+    useEffect(() => {
+        getIngred();
+      }, []);
+
+    function getDateTime() {
+        var now     = new Date(); 
+        var year    = now.getFullYear();
+        var month   = now.getMonth()+1; 
+        var day     = now.getDate();
+        if(month.toString().length === 1) {
+             month = '0'+month;
+        }
+        if(day.toString().length === 1) {
+             day = '0'+day;
+        }     
+        var dateTime = month + '-' + day + '-' + year;   
+         return dateTime;
+    }
+
+    function createIngredient(){
+        var id = getID();
+        console.log(id);
+        var ingredName = document.getElementById("ingredName").value;
+        if(ingredName === ""){
+            document.getElementById("message").innerHTML = "No name entered, cancelling ingredient creation";
+            reset();
+            return;
+        }
+        var ingredQty = document.getElementById("ingredQty").value;
+        if(ingredQty === ""){
+            document.getElementById("message").innerHTML = "No price entered, cancelling ingredient creation";
+            reset();
+            return;
+        }
+        var storageLocation = document.getElementById("ingredLoc").value;
+        var orderDate = getDateTime();
+        var expire = document.getElementById("expire").value;
+        var expireDate = expire.substring(5,expire.length) + "-" + expire.substring(0,4);
+        if(expireDate === "-"){
+            document.getElementById("message").innerHTML = "No expiration date entered, cancelling ingredient creation";
+            reset();
+            return;
+        }
+        var out = id + ", 't', " + ingredQty + ", '" + ingredName + "', '" + storageLocation + "', '" + expireDate + "', '";
+        out += orderDate + "'";
+
+        var create = "INSERT INTO ingredients VALUES (" + out + ");";
+        updateData(create);
+        document.getElementById("message").innerHTML = "New ingredient with name " + ingredName + " has been created!";
+        reset();
+    }
+
+    function reset(){
+        document.getElementById("ingredName").value = "";
+        document.getElementById("ingredQty").value = "";
+        document.getElementById("expire").value = "";
+    }
+
+    function getID(){
+        while(true){
+            var id = Math.floor(Math.random() * 2000);
+            var final = ingredients.length - 1;
+            for(var i = 0; i < ingredients.length; i++){
+                if(ingredients[i].ingred_id === id){
+                    break;
+                }
+                if(i === final && ingredients[final].ingred_id !== id){
+                    return id;
+                }
+            }
+        }
+    }
 
     return (
         <div>
             <label>Add New Ingredient:</label><br/>
+            <p id = "message"></p>
+            <button
+                onClick={() => {
+                    createIngredient();
+                }}
+            >
+                Create New Ingredient
+            </button><br/>
+            <input type = "text" id = "ingredName" placeholder = "Ingredient name"/>
+            <input type = "number" id = "ingredQty" placeholder = "Ingredient Quantity"/>
+            <br/>
+            <label>  Storage Location:</label>
+            <select id = "ingredLoc">
+                <option>cupboard</option>
+                <option>refrigerator</option>
+                <option>freezer</option>
+            </select>
+            <br/>
+            <labeL>Expiration Date:</labeL><input type = "date" id = "expire"/>
+            <br/>
+            <br/>
             <button
                 onClick={() => {
                     navigate("/manager");
